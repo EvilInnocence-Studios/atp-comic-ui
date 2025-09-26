229b30627d9bbcdec3f871491a3d5e8bc97a5252
Andrew Wittrock <andy@wittrock.us>
2025-09-25 23:50:59 -0500

    Add latest page to homepage

--- components/LatestPage/LatestPage.component.tsx
+++ components/LatestPage/LatestPage.component.tsx
@@ -1,6 +1,8 @@
+import { BlueskyFeed } from "@common/components/BlueskyFeed";
 import { PageView } from "../PageView";
-import {LatestPageProps} from "./LatestPage.d";
+import { LatestPageProps } from "./LatestPage.d";
 
 export const LatestPageComponent = ({pageUrl}:LatestPageProps) => <>
     {pageUrl && <PageView url={pageUrl} />}
+    <BlueskyFeed />
 </>;
---
+++ index.tsx
@@ -0,0 +1,19 @@
+import { useSetting } from "@common/lib/setting/services";
+import { IModule } from "@core/lib/module";
+import { sitePlugins } from "@public/index";
+import { LatestPage } from "./components/LatestPage";
+import { comicMenus } from "./lib/menus";
+import { comicRoutes } from "./lib/routes";
+import { comicSettings } from "./lib/settings";
+
+export const module:IModule = {
+    name: "comic",
+    menus: comicMenus,
+    routes: comicRoutes,
+    settings: comicSettings,
+}
+
+sitePlugins.homepage.register(75, () => {
+    const showLatestPage = useSetting("homepage.showLatestComicPage") === "true";
+    return showLatestPage ? <LatestPage /> : null;
+})
