72097e69e22b18929be910c94073f28aac23d133
Andrew Wittrock <andy@wittrock.us>
2025-10-16 21:09:10 -0500

    Move news to right sidebar

--- components/LatestPage/LatestPage.component.tsx
+++ components/LatestPage/LatestPage.component.tsx
@@ -6,12 +6,12 @@ import styles from "./LatestPage.module.scss";
 
 export const LatestPageComponent = ({pageUrl}:LatestPageProps) => <>
     <Row gutter={16}>
+        <Col xs={18}>
+            {pageUrl && <PageView url={pageUrl} />}
+        </Col>
         <Col xs={6} className={styles.newsFeed}>
             <h2>News Feed</h2>
             <BlueskyFeed pageSize={5} />
-        </Col>
-        <Col xs={18}>
-            {pageUrl && <PageView url={pageUrl} />}
         </Col>
     </Row>    
 </>;
