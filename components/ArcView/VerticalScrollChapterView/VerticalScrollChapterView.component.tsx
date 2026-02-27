9a6e84861e5c544d0d7fab09633ee2a73bf9ebbf
Andrew Wittrock <andy@wittrock.us>
2026-02-27 13:37:23 -0600

    Fix formatting issues on vertical scroll chapter view

--- components/ArcView/VerticalScrollChapterView/VerticalScrollChapterView.component.tsx
+++ components/ArcView/VerticalScrollChapterView/VerticalScrollChapterView.component.tsx
@@ -9,24 +9,24 @@ import { Date } from "@core/components/Date";
 
 export const VerticalScrollChapterViewComponent = overridable(({chapters, arc, classes = styles}:VerticalScrollChapterViewProps) =>
     <div className={classes.verticalScrollChapters}>
-        <h2>{arc?.name}</h2>
+        <h2 className={classes.arcName}>{arc?.name}</h2>
         <ul>
             {chapters.map(chapter =>
                 <li key={chapter.id}>
                     <Row gutter={16} align="middle">
-                        <Col span={4}>
+                        <Col xs={6} sm={5} md={4}>
                             <Link to={`/comic/arc/${chapter.url}`}>
                                 {!!chapter.thumbnailUrl && <ComicImage fileName={chapter.thumbnailUrl} className={classes.thumbnail} />}
                             </Link>
                         </Col>
-                        <Col span={18}>
+                        <Col xs={16} sm={17} md={18}>
                             <Row gutter={16}>
-                                <Col span={16}>
+                                <Col md={16} xs={24}>
                                     <h2><Link to={`/comic/arc/${chapter.url}`}>
                                         {chapter.name}
                                     </Link></h2>
                                 </Col>
-                                <Col span={8}>
+                                <Col md={8} xs={24}>
                                     <Date date={chapter.postDate} noWeekday />
                                 </Col>
                                 <Col span={24}>
