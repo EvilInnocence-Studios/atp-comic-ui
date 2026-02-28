b6128e1cda46da4e5249cce30207c8f1199570b5
Andrew Wittrock <andy@wittrock.us>
2026-02-28 00:03:58 -0600

    Fix page nav icon size

--- components/PageNav/PageNav.component.tsx
+++ components/PageNav/PageNav.component.tsx
@@ -16,42 +16,40 @@ export const PageNavComponent = overridable(({
     classes = styles
 }: PageNavProps) =>
     <div className={clsx([classes.comicNavLinksContainer, top && classes.top, bottom && classes.bottom])} style={{ textAlign: "center", fontSize: "x-large" }}>
-        <div>
-            {firstPage && prevPage && <Link to={`/comic/page/${(firstPage.url)}`}>
+        <div className={classes.navItem}>
+            <Link className={clsx(!prevPage && classes.noShow)} to={`/comic/page/${(firstPage?.url)}`}>
                 {["icon", "text"].includes(linkType) && <FontAwesomeIcon icon={faAngleDoubleLeft} />}
                 {["text"].includes(linkType) && " First"}
                 {["image"].includes(linkType) && <MediaImage settingKey="comic.firstPageIcon" />}
-            </Link>}
-            {firstPage && firstPage.imageUrl && <ComicImage className={classes.noShow} fileName={firstPage.imageUrl} />}
+            </Link>
         </div>
-        <div>
-            {prevPage && <Link to={`/comic/page/${prevPage.url}`}>
+        <div className={classes.navItem}>
+            <Link className={clsx(!prevPage && classes.noShow)} to={`/comic/page/${prevPage?.url}`}>
                 {["icon", "text"].includes(linkType) && <FontAwesomeIcon icon={faAngleLeft} />}
                 {["text"].includes(linkType) && " Previous"}
                 {["image"].includes(linkType) && <MediaImage settingKey="comic.previousPageIcon" />}
-            </Link>}
-            {prevPage && prevPage.imageUrl && <ComicImage className={classes.noShow} fileName={prevPage.imageUrl} />}
+            </Link>
         </div>
-        {arc && <div><Link to={`/comic/arc/${arc.url}`}>
-            {["icon"].includes(linkType) && <FontAwesomeIcon icon={faBoxArchive} />}
-            {["text"].includes(linkType) && `Archives`}
-            {["image"].includes(linkType) && <MediaImage settingKey="comic.archiveIcon" />}
-        </Link></div>}
-        <div>
-            {nextPage && <Link to={`/comic/page/${nextPage.url}`}>
+        <div className={classes.navItem}>
+            <Link className={clsx(!arc && classes.noShow)} to={`/comic/arc/${arc?.url}`}>
+                {["icon"].includes(linkType) && <FontAwesomeIcon icon={faBoxArchive} />}
+                {["text"].includes(linkType) && `Archives`}
+                {["image"].includes(linkType) && <MediaImage settingKey="comic.archiveIcon" />}
+            </Link>
+        </div>
+        <div className={classes.navItem}>
+            <Link className={clsx(!nextPage && classes.noShow)} to={`/comic/page/${nextPage?.url}`}>
                 {["text"].includes(linkType) && " Next"}
                 {["icon", "text"].includes(linkType) && <FontAwesomeIcon icon={faAngleRight} />}
                 {["image"].includes(linkType) && <MediaImage settingKey="comic.nextPageIcon" />}
-            </Link>}
-            {nextPage && nextPage.imageUrl && <ComicImage className={classes.noShow} fileName={nextPage.imageUrl} />}
+            </Link>
         </div>
-        <div>
-            {lastPage && nextPage && <Link to={`/comic/page/${lastPage.url}`}>
+        <div className={classes.navItem}>
+            <Link className={clsx(!nextPage && classes.noShow)} to={`/comic/page/${lastPage?.url}`}>
                 {["text"].includes(linkType) && " Last"}
                 {["icon", "text"].includes(linkType) && <FontAwesomeIcon icon={faAngleDoubleRight} />}
                 {["image"].includes(linkType) && <MediaImage settingKey="comic.lastPageIcon" />}
-            </Link>}
-            {lastPage && lastPage.imageUrl && <ComicImage className={classes.noShow} fileName={lastPage.imageUrl} />}
+            </Link>
         </div>
     </div>
 );
