1790cc34d359140b260a0385b48b08c58e36a599
Andrew Wittrock <andy@wittrock.us>
2026-02-28 00:15:20 -0600

    Stack page title and transcript on mobile

--- components/PageView/PageView.component.tsx
+++ components/PageView/PageView.component.tsx
@@ -9,6 +9,7 @@ import { PageViewProps } from "./PageView.d";
 import styles from './PageView.module.scss';
 import { overridable } from "@core/lib/overridable";
 import clsx from "clsx";
+import { Col, Row } from "antd";
 
 export const PageViewComponent = overridable(({ page, pageNumber, nextPage, transcript, classes = styles, className }: PageViewProps) => <>
     {!!page && <div className={clsx(classes.comicPage, className)}>
@@ -16,15 +17,17 @@ export const PageViewComponent = overridable(({ page, pageNumber, nextPage, tran
         {!!nextPage && <Link to={`/comic/page/${nextPage.url}`}><ComicImage fileName={page.imageUrl || ""} /></Link>}
         {!nextPage && <ComicImage fileName={page.imageUrl || ""} />}
         <PageNav page={page} bottom />
-        <div className={classes.pageDetails}>
-            <h1>Page {pageNumber}: {page.name}</h1>
-            <p><Date date={page.postDate} /></p>
-        </div>
-        <div className={classes.pageTranscript}>
-            <h1 onClick={transcript.toggle}><FontAwesomeIcon icon={faClosedCaptioning} /> Transcript</h1>
-            {transcript.isset && <div className={classes.transcriptCopy}>
-                <Markdown>{page.transcript}</Markdown>
-            </div>}
-        </div>
+        <Row gutter={8}>
+            <Col xs={24} md={12} className={classes.pageDetails}>
+                <h1>Page {pageNumber}: {page.name}</h1>
+                <p><Date date={page.postDate} /></p>
+            </Col>
+            <Col xs={24} md={12} className={classes.pageTranscript}>
+                <h1 onClick={transcript.toggle}><FontAwesomeIcon icon={faClosedCaptioning} /> Transcript</h1>
+                {transcript.isset && <div className={classes.transcriptCopy}>
+                    <Markdown>{page.transcript}</Markdown>
+                </div>}
+            </Col>
+        </Row>
     </div>}
 </>);
