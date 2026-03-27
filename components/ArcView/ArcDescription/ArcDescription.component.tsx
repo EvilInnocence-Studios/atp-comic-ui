da26208e7bdc7e403535df7bacee5034740f615a
Andrew Wittrock <andy@wittrock.us>
2026-03-27 15:09:28 -0500

    Add arc description layout component

--- components/ArcView/ArcDescription/ArcDescription.component.tsx
+++ components/ArcView/ArcDescription/ArcDescription.component.tsx
@@ -1,9 +1,8 @@
 import { overridable } from "@core/lib/overridable";
-import {ArcDescriptionProps} from "./ArcDescription.d";
-import styles from './ArcDescription.module.scss';
+import { ArcDescriptionProps } from "./ArcDescription.d";
 
-export const ArcDescriptionComponent = overridable(({classes = styles, slots, __layoutId, className, css, name}:ArcDescriptionProps) => <>
+export const ArcDescriptionComponent = overridable(({className, css, arc}:ArcDescriptionProps) => <>
     {css && <style>{css}</style>}
-    <div className={className}>ArcDescription component goes here.</div>
+    <div className={className}>{arc?.summary}</div>
 </>);
 
