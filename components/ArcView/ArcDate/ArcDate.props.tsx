9ed23b416eb30bd822dfad326a0ef26230de64ba
Andrew Wittrock <andy@wittrock.us>
2026-03-28 14:04:21 -0500

    Add format dropdown to arc data component

--- components/ArcView/ArcDate/ArcDate.props.tsx
+++ components/ArcView/ArcDate/ArcDate.props.tsx
@@ -1,6 +1,25 @@
 import { Label } from "@core/components/Label";
 import { IArcDateInputProps } from "./ArcDate.d";
 import { Editable } from "@core/components/Editable";
+import { Select } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestion } from "@fortawesome/free-solid-svg-icons";
+
+const formatOptions = [
+    {value: "", label: "Custom"},
+    {value: "MMMM D, YYYY", label: "January 1, 2026"},
+    {value: "MMM D, YYYY", label: "Jan 1, 2026"},
+    {value: "dddd, MMMM D, YYYY", label: "Thursday, January 1, 2026"},
+    {value: "ddd, MMM D, YYYY", label: "Thu, Jan 1, 2026"},
+    {value: "MM/DD/YYYY", label: "01/01/2026 (US)"},
+    {value: "DD/MM/YYYY", label: "01/01/2026 (UK)"},
+    {value: "YYYY-MM-DD", label: "2026-01-01 (ISO)"},
+    {value: "MMMM YYYY", label: "January 2026"},
+    {value: "MMMM D", label: "January 1"},
+    {value: "MMM D", label: "Jan 1"},
+    {value: "dddd", label: "Thursday"},
+    {value: "YYYY", label: "2026"},
+]
 
 export const ArcDatePropEditor = (
     {url, format}: IArcDateInputProps,
@@ -20,7 +39,17 @@ export const ArcDatePropEditor = (
                 onChange={updateProp("format")}
                 placeholder="MMMM D, YYYY"
             />
+            <Select
+                value={format || ""}
+                onChange={updateProp("format")}
+                placeholder="Custom"
+                options={formatOptions}
+            />
         </Label>
-        <a href="https://day.js.org/docs/en/display/format" target="_blank">Day.js Format</a>
+        <div style={{textAlign: "right"}}>
+            <a href="https://day.js.org/docs/en/display/format" target="_blank">
+                <FontAwesomeIcon icon={faQuestion} /> Date format help
+            </a>
+        </div>
     </>;
 }
