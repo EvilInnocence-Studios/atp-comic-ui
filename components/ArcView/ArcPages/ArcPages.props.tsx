17509ec13d93ae3727bafb5560bd1046c15a5e84
Andrew Wittrock <andy@wittrock.us>
2026-04-01 23:34:38 -0500

    Fix typescript errors

--- components/ArcView/ArcPages/ArcPages.props.tsx
+++ components/ArcView/ArcPages/ArcPages.props.tsx
@@ -1,11 +1,19 @@
+import { Editable } from "@core/components/Editable";
+import { Label } from "@core/components/Label";
 import { IArcPagesInputProps } from "./ArcPages.d";
 
 export const ArcPagesPropEditor = (
-    {}: IArcPagesInputProps,
-    updateProps: (props: any) => void,
+    {url}: IArcPagesInputProps,
+    _updateProps: (props: any) => void,
     updateProp: (prop: string) => (value: any) => void
 ) => {
     return <>
-        Placeholder Prop Editor for ArcPages
+        <Label label="Url">
+            <Editable
+                value={url || ""}
+                onChange={updateProp("url")}
+            />
+        </Label>
+        
     </>;
 }
