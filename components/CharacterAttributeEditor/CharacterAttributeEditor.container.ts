e77f5ab95753ce5613994f3c838413a21a8e3671
Andrew Wittrock <andy@wittrock.us>
2026-04-17 15:28:42 -0500

    Remove tab

--- components/CharacterAttributeEditor/CharacterAttributeEditor.container.ts
+++ components/CharacterAttributeEditor/CharacterAttributeEditor.container.ts
@@ -11,68 +11,68 @@ import { CharacterAttributeEditorComponent } from "./CharacterAttributeEditor.co
 import { CharacterAttributeEditorProps, ICharacterAttributeEditorInputProps, ICharacterAttributeEditorProps } from "./CharacterAttributeEditor.d";
 
 const injectCharacterAttributeEditorProps = createInjector(({characterId}:ICharacterAttributeEditorInputProps):ICharacterAttributeEditorProps => {
-        const [attributes, setAttributes] = useState<ICharacterAttribute[]>([]);
-        const loader =  useLoaderAsync();
-    
-        const att = services().character.attribute;
-    
-        const update = (id:string, field:keyof ICharacterAttribute) => (value:any) => {
-            const oldAtts = attributes;
-            setAttributes(attributes.map(a => a.id === id ? {...a, [field]: value} : a));
-            loader(() => att.update(characterId, id, { [field]: value })
-                .then(flash.success("Attribute updated"))
-                .catch(all(() => setAttributes(oldAtts), flash.error("Failed to update attribute")))
-            );
-        }
-    
-        const remove = (id:string) => () => {
-            const oldAtts = attributes;
-            setAttributes(attributes.filter(a => a.id !== id));
-            loader(() => att.remove(id, id)
-                .then(flash.success("Attribute removed"))
-                .catch(all(() => setAttributes(oldAtts), flash.error("Failed to remove attribute")))
-            );
-        }
-    
-        const [name, setName] = useState('');
-        const [value, setValue] = useState('');
-        const create = () => {
-            loader(() => att.create(characterId, {
-                name,
-                value,
-                sortOrder: Math.max(0, ...attributes.map(a => a.sortOrder)) + 1,
-            })
-                .then(appendTo(attributes))
-                .then(all(
-                    refresh,
-                    flash.success(`Attribute ${name} created`),
-                    clear(setName),
-                    clear(setValue),
-                ))
-                .catch(flash.error("Failed to create attribute"))
-            );
-        }
-    
-        const sortAtts = (attId: string, newIndex: number) => {
-            loader(() => att.sort(characterId, attId, newIndex)
-                .then(refresh)
-                .catch(flash.error("Failed to sort attributes"))
-            );
-        }
-    
-        const refresh = () => {
-            loader(() => att.search(characterId)
-                .then(pipe(sort(sort.by<ICharacterAttribute>(prop("sortOrder")).asc), setAttributes))
-                .catch(flash.error("Failed to load attributes"))
-            );
-        };
-        useEffect(refresh, [characterId]);
-    
-        return {
-            attributes, isLoading: loader.isLoading,
-            name, setName, value, setValue,
-            create, update, remove, sort: sortAtts,
-        };
+    const [attributes, setAttributes] = useState<ICharacterAttribute[]>([]);
+    const loader =  useLoaderAsync();
+
+    const att = services().character.attribute;
+
+    const update = (id:string, field:keyof ICharacterAttribute) => (value:any) => {
+        const oldAtts = attributes;
+        setAttributes(attributes.map(a => a.id === id ? {...a, [field]: value} : a));
+        loader(() => att.update(characterId, id, { [field]: value })
+            .then(flash.success("Attribute updated"))
+            .catch(all(() => setAttributes(oldAtts), flash.error("Failed to update attribute")))
+        );
+    }
+
+    const remove = (id:string) => () => {
+        const oldAtts = attributes;
+        setAttributes(attributes.filter(a => a.id !== id));
+        loader(() => att.remove(id, id)
+            .then(flash.success("Attribute removed"))
+            .catch(all(() => setAttributes(oldAtts), flash.error("Failed to remove attribute")))
+        );
+    }
+
+    const [name, setName] = useState('');
+    const [value, setValue] = useState('');
+    const create = () => {
+        loader(() => att.create(characterId, {
+            name,
+            value,
+            sortOrder: Math.max(0, ...attributes.map(a => a.sortOrder)) + 1,
+        })
+            .then(appendTo(attributes))
+            .then(all(
+                refresh,
+                flash.success(`Attribute ${name} created`),
+                clear(setName),
+                clear(setValue),
+            ))
+            .catch(flash.error("Failed to create attribute"))
+        );
+    }
+
+    const sortAtts = (attId: string, newIndex: number) => {
+        loader(() => att.sort(characterId, attId, newIndex)
+            .then(refresh)
+            .catch(flash.error("Failed to sort attributes"))
+        );
+    }
+
+    const refresh = () => {
+        loader(() => att.search(characterId)
+            .then(pipe(sort(sort.by<ICharacterAttribute>(prop("sortOrder")).asc), setAttributes))
+            .catch(flash.error("Failed to load attributes"))
+        );
+    };
+    useEffect(refresh, [characterId]);
+
+    return {
+        attributes, isLoading: loader.isLoading,
+        name, setName, value, setValue,
+        create, update, remove, sort: sortAtts,
+    };
 });
 
 const connect = inject<ICharacterAttributeEditorInputProps, CharacterAttributeEditorProps>(mergeProps(
